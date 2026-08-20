666a2364720d19573e867a52b3fee05a1475c940
111bayraktar-cmyk <111bayraktar@gmail.com>
2026-08-20 01:44:28 +0000

    Lab5

---
+++ ibayraktar5.py
@@ -0,0 +1,35 @@
+#ibayraktar5; Lists in Python
+
+nums = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+
+print (nums[0])
+print (nums[1])
+print (nums[2])
+print (nums[3])
+print (nums[4])
+print (nums[5])
+print (nums[6])
+print (nums[7])
+print (nums[8])
+print (nums[9])
+
+print (" ")
+for i in nums:
+    print (i)
+
+print (" ")
+yummy = ["apple", "lemon", "garlic", "kiwi", "orange"]
+
+for i in yummy:
+    print (i)
+
+
+
+
+
+
+
+
+
+
+
