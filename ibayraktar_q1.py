b13f98a7417d25f6f3df2f21b96e2c3aab4cdb23
111bayraktar-cmyk <111bayraktar@gmail.com>
2026-09-10 00:15:11 +0000

    Question 1

---
+++ ibayraktar_q1.py
@@ -0,0 +1,10 @@
+#ibayraktar_question1
+
+name=input("Enter your name: ")
+number=int(input("enter a number: "))
+addn=int(input("How much do you want to add to your number?"))
+total=number+addn
+print(name, "you entered ",number, "and added ", addn, "to get a total of ", total)
+
+
+
