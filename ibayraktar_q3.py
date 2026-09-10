3ba72f3621a1c419efe095c3d7032edc3087c346
111bayraktar-cmyk <111bayraktar@gmail.com>
2026-09-10 00:30:52 +0000

    Question 3

---
+++ ibayraktar_q3.py
@@ -0,0 +1,16 @@
+#ibayraktar_question 3
+
+def area(base,height):
+    area=base * height
+    return area
+
+a = int(input("Enter the base length of the rectangle: "))
+
+b = int(input("Enter the height length of the rectangle: "))
+
+result =area (a, b)
+
+print ("The area of the rectangle is ", result)
+
+
+
